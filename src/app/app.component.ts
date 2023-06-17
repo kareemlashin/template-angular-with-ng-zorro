dcf20d847cdc0eccb0f0b4e514604404d1b11f9b
Kareem Elsayed <kelsayed.c@saip.gov.sa>
2023-06-17 13:31:37 +0300

    init lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,54 @@ import { NzButtonSize } from 'ng-zorro-antd/button';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {
 
-  }
   size: any = 'large';
+  isCollapsed = false;
+  current = 0;
 
+  index = 'First-content';
+
+  pre(): void {
+    this.current -= 1;
+    this.changeContent();
+  }
+
+  next(): void {
+    this.current += 1;
+    this.changeContent();
+  }
+
+  done(): void {
+    console.log('done');
+  }
+
+  changeContent(): void {
+    switch (this.current) {
+      case 0: {
+        this.index = 'First-content';
+        break;
+      }
+      case 1: {
+        this.index = 'Second-content';
+        break;
+      }
+      case 2: {
+        this.index = 'third-content';
+        break;
+      }
+      default: {
+        this.index = 'error';
+      }
+    }
+  }
+
+  inputValue?: string;
+  filteredOptions: string[] = [];
+  options = ['Burns Bay Road', 'Downing Street', 'Wall Street'];
+  constructor() {
+    this.filteredOptions = this.options;
+  }
+  onChange(value: string): void {
+    this.filteredOptions = this.options.filter(option => option.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+  }
 }
